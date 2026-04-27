d14bf361cedf34cc3e34eaefdd240ec888bc9a70
Akila Gavin <akilagavinkatudampe7@gmail.com>
2026-04-27 12:22:59 +0530

    chore: finalize merge and clean up routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 // 2. Search Route 
 // IMPORTANT: This must be ABOVE the resource route so "search" isn't treated as an ID
 Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
@@ -20,17 +19,7 @@ Route::resource('tickets', TicketController::class)->scoped([
 ]);
 
 // 3. Resource Routes for Tickets
-// This automatically creates routes for index, create, store, show, etc.
-// We use 'scoped' so the URL uses the 'ref' instead of the numeric 'id'
-=======
-// 2. Custom Search Route
-// Using GET for search is standard so users can bookmark their results page
-Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
 
-// 3. Resource Routes for Tickets
-// We use 'scoped' so Laravel knows to look for the 'ref' column in the database
-// instead of the default 'id' when visiting /tickets/{ticket}
->>>>>>> feature/add-ticket-controller
 Route::resource('tickets', TicketController::class)->scoped([
     'ticket' => 'ref',
 ]);
