5e88f55c9988df5ad847896b80155e0d30f9eb0e
Akila Gavin <akilagavinkatudampe7@gmail.com>
2026-04-27 08:53:43 +0530

    refactor: clean up duplicate routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 // 2. Search Route 
 // IMPORTANT: This must be ABOVE the resource route so "search" isn't treated as an ID
 Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
+
 
 // 3. Resource Routes
 // 'scoped' tells Laravel to look up the Ticket by the 'ref' column in the database
 Route::resource('tickets', TicketController::class)->scoped([
     'ticket' => 'ref',
 ]);
+
 
 // 3. Resource Routes for Tickets
 // This automatically creates routes for index, create, store, show, etc.
