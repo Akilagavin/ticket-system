c694b046c4befff8ba8b7ddd09959b8a439e0004
Akila Gavin <akilagavinkatudampe7@gmail.com>
2026-04-29 09:38:39 +0530

    feat: finalize search routing with scoped ticket references

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ Route::get('/', function () {
 Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
 
 
-// 3. Resource Routes
-// 'scoped' tells Laravel to look up the Ticket by the 'ref' column in the database
-Route::resource('tickets', TicketController::class)->scoped([
-    'ticket' => 'ref',
-]);
 
 
 // 3. Resource Routes for Tickets
