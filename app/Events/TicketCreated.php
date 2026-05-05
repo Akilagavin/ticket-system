6c28639d2bcd4a71d57e19283389b36718cb696c
Akila Gavin <akilagavinkatudampe7@gmail.com>
2026-05-05 21:49:52 +0530

    fix: add public ticket property to TicketCreated event

---
+++ app/Events/TicketCreated.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Ticket;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TicketCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The ticket instance.
+     * Making this public allows listeners like SendNewTicketMail to access it.
+     * 
+     * @var \App\Models\Ticket
+     */
+    public $ticket;
+
+    /**
+     * Create a new event instance.
+     * 
+     * @param \App\Models\Ticket $ticket
+     * @return void
+     */
+    public function __construct(Ticket $ticket)
+    {
+        $this->ticket = $ticket;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
